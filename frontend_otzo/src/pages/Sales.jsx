ddbbd7e381c532865ccbc154b8566214c6b747ad
ElGg01 <jcng.aa135@gmail.com>
2024-10-24 19:21:59 -0600

    GRID

--- frontend_otzo/src/pages/Sales.jsx
+++ frontend_otzo/src/pages/Sales.jsx
@@ -8,6 +8,29 @@ export function Sales() {
         <div>
           <motion.h1 initial={{scale: 0}} animate={{scale: 1, transition: {delay: 0.5}}} className='text-center text-white text-8xl font-bold'>Ventas</motion.h1>
         </div>
+        <div className='grid grid-cols-1 grid-rows-1 gap-x-2.5 md:grid-cols-2'>
+          <div className='w-full h-full p-4'>
+            <div className='w-full h-full bg-white rounded-lg p-4'>
+              <div className='shadow-lg rounded-lg p-4 bg-rose-400'>
+                <h2 className='font-bold'>Productos</h2>
+              </div>
+              <div className='h-[calc(20vh)] md:h-[calc(50vh)]'>
+
+              </div>
+            </div>
+          </div>
+          
+          <div className='w-full h-full p-4'>
+            <div className='w-full h-full bg-white rounded-lg p-4'>
+              <div className='shadow-lg rounded-lg p-4 bg-yellow-500'>
+                <h2 className='font-bold'>Carrito</h2>
+              </div>
+              <div className='h-[calc(20vh)] md:h-[calc(50vh)]'>
+
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   )
