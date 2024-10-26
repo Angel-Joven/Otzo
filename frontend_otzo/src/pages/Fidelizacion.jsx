5ea92b40cdf745e01168695b71f190f73a299ec6
Angel Joven <cchangeljoven@gmail.com>
2024-10-25 19:08:00 -0600

    pagina fidelizacion actualizada al nuevo diseño base

--- frontend_otzo/src/pages/Fidelizacion.jsx
+++ frontend_otzo/src/pages/Fidelizacion.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import { motion } from 'framer-motion';
 
 export function Fidelizacion() {
-  return (
+   return (
     <>
-      <div className='bg-gradient-to-r from-purple-400 to-purple-500 w-full h-full overflow-x-hidden fixed -z-10'></div>
-      <div>
-        <motion.h1 initial={{scale: 0}} animate={{scale: 1, transition: {delay: 0.5}}} className='text-center text-white text-8xl font-bold'>Fidelizacion y Marketing</motion.h1>
+      <div className='bg-gradient-to-r from-purple-400 to-purple-500 w-full h-full min-h-[calc(100vh-5rem)] z-0 relative'>
+        <div>
+          <motion.h1 initial={{scale: 0}} animate={{scale: 1, transition: {delay: 0.5}}} className='text-center text-white text-6xl font-bold'>Fidelizacion y Marketing</motion.h1>
+        </div>
       </div>
     </>
   )
