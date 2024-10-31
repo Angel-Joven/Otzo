d0549a1e2b7506bba8d93d09a6bb2129e0f7756f
ElGg01 <jcng.aa135@gmail.com>
2024-10-30 22:57:19 -0600

    Mejora de la organizacion

--- backend_otzo/src/__init__.py
+++ backend_otzo/src/__init__.py
@@ -1,5 +1,4 @@
 from flask import Flask
-from .db import get_connection
 
 app = Flask(__name__)
 
@@ -7,7 +6,5 @@ app = Flask(__name__)
 def init_app(config):
     # Configuration
     app.config.from_object(config)
-
-    db = get_connection()
 
     return app
