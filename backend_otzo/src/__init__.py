98303723cda62712aa18bdcd221146141349af49
ElGg01 <jcng.aa135@gmail.com>
2024-10-30 23:19:25 -0600

    Blueprint de ventas

--- backend_otzo/src/__init__.py
+++ backend_otzo/src/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from .routes import ventas
 
 app = Flask(__name__)
 
@@ -6,5 +7,8 @@ app = Flask(__name__)
 def init_app(config):
     # Configuration
     app.config.from_object(config)
+
+    # Blueprints
+    app.register_blueprint(ventas.ventas_bp, url_prefix="/api/ventas/")
 
     return app
