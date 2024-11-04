3fb17288f668043bdf4cca7a720b41c10d58d82e
ElGg01 <jcng.aa135@gmail.com>
2024-11-03 18:15:20 -0600

    CORS backend

--- backend_otzo/index.py
+++ backend_otzo/index.py
@@ -1,9 +1,11 @@
 from src.tests.test_db_connection import test_connection
 from config import config
 from src import init_app
+from flask_cors import CORS
 
 configuration = config["development"]
 app = init_app(configuration)
+CORS(app, resources={r"*": {"origins": "*"}})
 
 test_connection()
 
