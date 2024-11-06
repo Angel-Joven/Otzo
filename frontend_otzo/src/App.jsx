ba3aa194a5d9d2d7503b6639fb72d37f19a55605
ElGg01 <jcng.aa135@gmail.com>
2024-11-05 18:55:50 -0600

    Correciones

--- frontend_otzo/src/App.jsx
+++ frontend_otzo/src/App.jsx
@@ -1,23 +1,23 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Index } from './pages/Index';
-import { Sales } from './pages/Sales';
-import { Fidelizacion } from './pages/Fidelizacion';
-import { Atencion } from './pages/Atencion';
-import './App.css';
-import { Navbar } from './components/Navbar';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Index } from "./pages/Index";
+import { Sales } from "./pages/Sales";
+import { Fidelizacion } from "./pages/Fidelizacion";
+import { Atencion } from "./pages/Atencion";
+import "./App.css";
+import { Navbar } from "./components/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
-    <Navbar />
+      <Navbar />
       <Routes>
-        <Route path='/' element={<Index />} />
-        <Route path='/sales' element={<Sales />} />
-        <Route path='/fidelizacion' element={<Fidelizacion />} />
-        <Route path='/atencion' element={<Atencion />} />
+        <Route path="/" element={<Index />} />
+        <Route path="/sales" element={<Sales />} />
+        <Route path="/fidelizacion" element={<Fidelizacion />} />
+        <Route path="/atencion" element={<Atencion />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App;
