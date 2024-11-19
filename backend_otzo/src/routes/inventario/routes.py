24f9385ea6b32278067abde4a6eaf869f061074e
ianflores7 <ian00space@gmail.com>
2024-11-19 01:21:29 -0600

    rutas de inventario

--- backend_otzo/src/routes/inventario/routes.py
+++ backend_otzo/src/routes/inventario/routes.py
@@ -9,3 +9,48 @@ from flask_cors import cross_origin
 
 from src.models.inventario.inventarioDTO import inventarioDTO
 
+def custom_json_serializer(obj):
+    if isinstance(obj, Decimal):
+        return float(obj)  
+    elif isinstance(obj, datetime):
+        return obj.isoformat()  
+    return str(obj)  
+
+
+@inventario_bp.route("/", methods=["GET"])
+def index():
+    conexion = get_connection()
+    cursor = conexion.cursor(DictCursor)
+
+    cursor.execute(
+        "SELECT * FROM inventario",
+    )
+
+    resultado = cursor.fetchall()
+    conexion.close()
+
+   json_data = json.dumps(
+        {"inventario": resultado}, ensure_ascii=False, default=custom_json_serializer
+    )
+    return Response(json_data, content_type="application/json; charset=utf-8")
+
+@inventario_bp.route("/test", methods=["POST", "OPTIONS"])
+@cross_origin()
+def test():
+    data = request.json
+
+    inventario = []
+
+    for producto in data["inventario"]:
+
+        # TODO: FUNCION QUE AL PASAR UN NOMBRE DE PRODUCTO Y LA CANTIDAD, RETORNE
+        # UNA LISTA DE DICCIONARIOS CON TODOS LOS CAMPOS POR CADA PRODUCTO Y SI NO HAY SUFICIENTE CANTIDAD
+        # QUE RETORNE NONE -----FUNCIONALIDAD DE INVENTARIO-----
+
+        print(producto)
+    
+
+    return jsonify({"Mensaje": "producto agregado correctamente"}), 200
+
+
+
