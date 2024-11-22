15aa81c166ec9ede6a43d36886565e54a873f35f
Leonardo <leoyforce777@gmail.com>
2024-11-21 23:27:55 -0600

    actualisacion de reportes

--- backend_otzo/src/routes/reportes/routes.py
+++ backend_otzo/src/routes/reportes/routes.py
@@ -4,7 +4,7 @@ from flask import request,jsonify
 from . import reportes_bp
 import json
 
-@reportes_bp.route('/api/reporte_diario', methods=['GET'])
+@reportes_bp.route('/reporte_diario', methods=['GET'])
 def obtener_reporte_diario():
     """Devuelve el último reporte generado."""
     reporte_db = obtener_reporte_mas_reciente()
