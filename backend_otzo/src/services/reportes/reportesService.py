4b19a9544853f50d1955a8967cb4ed0489387f09
Leonardo <leoyforce777@gmail.com>
2024-11-22 00:05:14 -0600

    actualacion reportes

--- backend_otzo/src/services/reportes/reportesService.py
+++ backend_otzo/src/services/reportes/reportesService.py
@@ -1,3 +1,4 @@
+from src.services.fidelizacion.fidelizacionService import ObtenerInfoClientesPuntosService
 from datetime import datetime
 from src.models.reportes.reportesModels import obtener_datos_puntos, guardar_reporte_diario
 from src.models.reportes.reportesDTO import ReporteDTO
@@ -15,3 +16,13 @@ def generar_reporte_diario():
     # Guardar el reporte en la base de datos
     guardar_reporte_diario(reporte)
     return reporte
+
+def crear_reporte_puntos(fecha_dia):
+    datos_puntos=ObtenerInfoClientesPuntosService().obtener_info_clientes_puntos()
+    clientes=datos_puntos[0]
+    puntos=datos_puntos[2]
+    ultima_fecha_actualizacion_puntos=datos_puntos[3]
+    print("prueba 1")
+    print(clientes)
+
+crear_reporte_puntos(fecha_dia="1")
