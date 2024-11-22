6f10abd49b073ea64dea60cb5ad081a695ca4386
ElGg01 <jcng.aa135@gmail.com>
2024-11-22 17:00:34 -0600

    Comentado de prints de reportes al iniciar el backend

--- backend_otzo/src/services/reportes/reportesService.py
+++ backend_otzo/src/services/reportes/reportesService.py
@@ -1,15 +1,18 @@
 from src.models.reportes.reportesModels import ReportesModelo
-from src.services.fidelizacion.fidelizacionService import ObtenerInfoClientesPuntosService
+from src.services.fidelizacion.fidelizacionService import (
+    ObtenerInfoClientesPuntosService,
+)
 from datetime import datetime
 from src.db import get_connection
 import json
+
 
 class ReportesService(ReportesModelo):
     @staticmethod
     def crear_reporte_puntos(fecha_reporte):
         """
         Genera un reporte de puntos filtrado por una fecha específica.
-        
+
         :param fecha_reporte: Fecha en formato 'YYYY-MM-DD' para filtrar los puntos.
         """
         datos_puntos = ObtenerInfoClientesPuntosService().obtener_info_clientes_puntos()
@@ -20,7 +23,9 @@ class ReportesService(ReportesModelo):
                 {
                     "idcliente_puntos": dato["idcliente_puntos"],
                     "total_puntos": dato["total_puntos"],
-                    "ultima_actualizacionPuntos": dato["ultima_actualizacionPuntos"].split("T")[0],
+                    "ultima_actualizacionPuntos": dato[
+                        "ultima_actualizacionPuntos"
+                    ].split("T")[0],
                 }
                 for dato in datos_puntos
                 if dato["ultima_actualizacionPuntos"].split("T")[0] == fecha_reporte
@@ -28,14 +33,17 @@ class ReportesService(ReportesModelo):
 
             # Si no hay datos para la fecha proporcionada
             if not datos_filtrados:
-                return {"mensaje": f"No se encontraron puntos para la fecha {fecha_reporte}"}
+                return {
+                    "mensaje": f"No se encontraron puntos para la fecha {fecha_reporte}"
+                }
 
             return datos_filtrados
         else:
             return {"error": datos_puntos}
 
-#prueba de funcionalidad fecha del reporte 
-print("Prueba con fecha específica")
-fecha_prueba = "2024-11-21"  # Cambia esta fecha para tus pruebas
-reporte = ReportesService.crear_reporte_puntos(fecha_prueba)
-print(reporte)
+
+# prueba de funcionalidad fecha del reporte
+# print("Prueba con fecha específica")
+# fecha_prueba = "2024-11-21"  # Cambia esta fecha para tus pruebas
+# reporte = ReportesService.crear_reporte_puntos(fecha_prueba)
+# print(reporte)
