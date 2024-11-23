f221d9bf2d435ba600991faaf5272f27e1ba81f7
ElGg01 <jcng.aa135@gmail.com>
2024-11-23 02:11:46 -0600

    UI inventario base

---
+++ frontend_otzo/src/components/inventario/ProductCard.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+export default function ProductCard({
+  imagen,
+  titulo,
+  categoria,
+  cantidad,
+  descripcion,
+}) {
+  return (
+    <div className="bg-slate-800 text-white p-2 w-64 rounded-xl">
+      <div className="relative">
+        <img src={imagen} alt="producto" className="w-24 m-auto" />
+        <p
+          className={`absolute top-0 right-0 p-2 font-bold ${
+            cantidad > 10 ? "text-green-500" : "text-red-500"
+          }`}
+        >
+          {cantidad}
+        </p>
+        <p className="absolute top-0 left-0 p-1 font-bold bg-rose-600 rounded-xl">
+          {categoria}
+        </p>
+      </div>
+      <div className="text-center">
+        <p className="font-bold text-xl truncate">{titulo}</p>
+        <p className="overflow-ellipsis truncate">{descripcion}</p>
+      </div>
+      <div className="flex justify-center gap-4 py-2">
+        <button className="bg-green-500 rounded-2xl p-2">
+          <i class="align-middle fi fi-br-plus"></i>
+        </button>
+        <button className="bg-blue-500 rounded-2xl p-2">
+          <i class="align-middle fi fi-sr-pencil"></i>
+        </button>
+        <button className="bg-red-500 rounded-2xl p-2">
+          <i class="align-middle fi fi-br-minus"></i>
+        </button>
+      </div>
+    </div>
+  );
+}
